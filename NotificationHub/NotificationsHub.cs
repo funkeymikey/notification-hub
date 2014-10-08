9b1fa7766ba424ba9ef03310f6d409cda91a043c
Mike Kozelsky <mike.kozelsky@gmail.com>
2014-10-07 22:10:23 -0400

    empty heartbeat method - trying this out

--- NotificationHub/NotificationsHub.cs
+++ NotificationHub/NotificationsHub.cs
@@ -5,6 +5,8 @@ namespace NotificationHub
     // this is empty because we only invoke the Clients.All.notify(Notification) from within the api controller
     public class NotificationsHub : Hub
     {
-        
+        public void Heartbeat()
+        {
+        }
     }
 }
